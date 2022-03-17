0ff85a21b0f4d2f7ad151a1342546c04e9f3c99e
Yannri Zenner <zenner88@gmail.com>
2022-03-17 23:32:57 +0700

    edit

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -68,6 +68,7 @@ export class DashboardComponent implements OnInit {
         return element.status == 1;
       })
       this.open = open.length;
+      console.log("open"+open);
       console.log("open"+open.length);
 
       let received = data.filter(element => {
@@ -162,36 +163,36 @@ export class DashboardComponent implements OnInit {
   // end maps 
 
 
-    this.datasets = [
-      [0, 20, 10, 30, 15, 40, 20, 60, 60],
-      [0, 20, 5, 25, 10, 30, 15, 40, 40]
-    ];
-    this.data = this.datasets[0];
+  //   this.datasets = [
+  //     [0, 20, 10, 30, 15, 40, 20, 60, 60],
+  //     [0, 20, 5, 25, 10, 30, 15, 40, 40]
+  //   ];
+  //   this.data = this.datasets[0];
 
 
-    var chartOrders = document.getElementById('chart-orders');
+  //   var chartOrders = document.getElementById('chart-orders');
 
-    parseOptions(Chart, chartOptions());
+  //   parseOptions(Chart, chartOptions());
 
 
-    var ordersChart = new Chart(chartOrders, {
-      type: 'bar',
-      options: chartExample2.options,
-      data: chartExample2.data
-    });
+  //   var ordersChart = new Chart(chartOrders, {
+  //     type: 'bar',
+  //     options: chartExample2.options,
+  //     data: chartExample2.data
+  //   });
 
-    var chartSales = document.getElementById('chart-sales');
+  //   var chartSales = document.getElementById('chart-sales');
 
-    this.salesChart = new Chart(chartSales, {
-			type: 'line',
-			options: chartExample1.options,
-			data: chartExample1.data
-		});
-  }
+  //   this.salesChart = new Chart(chartSales, {
+	// 		type: 'line',
+	// 		options: chartExample1.options,
+	// 		data: chartExample1.data
+	// 	});
+  // }
 
-  public updateOptions() {
-    this.salesChart.data.datasets[0].data = this.data;
-    this.salesChart.update();
-  }
+  // public updateOptions() {
+  //   this.salesChart.data.datasets[0].data = this.data;
+  //   this.salesChart.update();
+  // }
 
-}
+}}
