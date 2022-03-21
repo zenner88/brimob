72f21bdbbe489e0f055fd9e7f2f9a0355c615418
Yannri Zenner <zenner88@gmail.com>
2022-03-21 23:49:56 +0700

    bg-dark

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -100,13 +100,13 @@ export class DashboardComponent implements OnInit {
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         styles: [
           {"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},
-          {"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},
+          {"featureType":"landscape","elementType":"all","stylers":[{"color":"#FCFCC1"}]},
           {"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},
           {"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},
           {"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},
           {"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},
           {"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},
-          {"featureType":"water","elementType":"all","stylers":[{"color":'#5e72e4'},{"visibility":"on"}]}]
+          {"featureType":"water","elementType":"all","stylers":[{"color":'#212529'},{"visibility":"on"}]}]
     }
     map = new google.maps.Map(map, mapOptions);
     markers.forEach(location => {
