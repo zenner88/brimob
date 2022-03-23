99c58810e7d65ed251f9e252bbf1d7278cb5ed4c
Yannri Zenner <zenner88@gmail.com>
2022-03-23 21:23:54 +0700

    adding bar chart

--- src/app/pages/chart/chart.component.ts
+++ src/app/pages/chart/chart.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js';
 
-
+// core components
+import {
+  chartOptions,
+  parseOptions,
+  chartExample1,
+  chartExample2
+} from "../../variables/charts";
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.scss']
 })
 export class ChartComponent implements OnInit {
-
+  public data: any;
+  public salesChart;
+  public clicked: boolean = true;
+  public clicked1: boolean = false;
 
   constructor() { }
 
   ngOnInit() {       
-    
+    var chartOrders = document.getElementById('chart-orders');
+
+      parseOptions(Chart, chartOptions());
+
+      var ordersChart = new Chart(chartOrders, {
+        type: 'bar',
+        options: chartExample2.options,
+        data: chartExample2.data
+      });
   }
 }
